4523f1123e9b14ca10aee3206a1839145e8738e6
Alan Moore <me@alandmoore.com>
2021-02-11 12:10:51 -0600

    Change RC form to auto-calculate

--- electronics_calculator/rc_frequency_form.py
+++ electronics_calculator/rc_frequency_form.py
@@ -15,9 +15,10 @@ class RCFrequencyForm(tk.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.columnconfigure(4, weight=1)
-        self.resistance_val = PrefixNumberVar(self, value=0.0, prefix='kilo')
-        self.capacitance_val = PrefixNumberVar(self, value=0.0, prefix='nano')
-        self.frequency_val = PrefixNumberVar(self, value=0.0, prefix='')
+        self.vars = dict()
+        self.vars['R'] = PrefixNumberVar(self, value=0.0, prefix='kilo')
+        self.vars['C'] = PrefixNumberVar(self, value=0.0, prefix='nano')
+        self.vars['F'] = PrefixNumberVar(self, value=0.0, prefix='')
         self.select_var = tk.StringVar(self)
         self.error_string = tk.StringVar(self, value='')
 
@@ -25,7 +26,7 @@ class RCFrequencyForm(tk.Frame):
 
         instructions = (
             'Select the value to be entered in the right column, '
-            '\nthen enter the other two values and hit "Calculate".'
+            '\nthen enter the other two values.'
         )
         ttk.Label(self, text=instructions).grid(
             row=0,
@@ -45,30 +46,29 @@ class RCFrequencyForm(tk.Frame):
         # track inputs for disabling
         self.inputs = dict()
         # Resistance
-        self.input_row(2, 'R', 'Resistance', 'Ohms', self.resistance_val)
+        self.input_row(2, 'R', 'Resistance', 'Ohms')
 
         # Capacitance
-        self.input_row(3, 'C', 'Capacitance', 'Farads', self.capacitance_val)
+        self.input_row(3, 'C', 'Capacitance', 'Farads')
 
         # Frequency
-        self.input_row(4, 'F', 'Frequency', 'Hertz', self.frequency_val)
+        self.input_row(4, 'F', 'Frequency', 'Hertz')
 
         # Error
         ttk.Label(self, textvariable=self.error_string).grid(row=10, sticky='ew', columnspan=5)
 
-        # Submit
-        ttk.Button(
-            self,
-            text='Calculate',
-            command=self.try_calculation
-        ).grid(row=99, column=4, sticky='w')
+        # set traces:
+        for var in self.vars.values():
+            var.trace_add('write', self._try_calculation)
+            var.prefix.trace_add('write', self._try_calculation)
+        self.select_var.trace_add('write', self._try_calculation)
 
+        # Set default.  Doing this here so triggers can happen
         self.select_var.set('F')
 
-    def input_row(self, row, key, label, unit, value_var):
+    def input_row(self, row, key, label, unit):
         """Create an input row"""
-        if not isinstance(value_var, PrefixNumberVar):
-            raise TypeError('value_var must be a PrefixNumberVar')
+
         # build the UI
         ttk.Radiobutton(
             self,
@@ -78,51 +78,41 @@ class RCFrequencyForm(tk.Frame):
         ).grid(row=row, column=0, sticky='ew')
         self.inputs[key] = Numberbox(
             self,
-            textvariable=value_var,
+            textvariable=self.vars[key],
             from_=0,
             increment=.01
         )
         self.inputs[key].grid(row=row, column=2, sticky='ew')
         ttk.OptionMenu(
             self,
-            value_var.prefix,
-            value_var.prefix.get() , *list(value_var.prefixes.keys())
+            self.vars[key].prefix,
+            self.vars[key].prefix.get() , *list(self.vars[key].prefixes.keys())
         ).grid(row=row, column=3, sticky='ew')
         ttk.Label(self, text=unit).grid(row=row, column=4, sticky='ew')
 
 
-    def try_calculation(self):
+    def _try_calculation(self, *_):
         """Attempt to calculate the missing value"""
 
-        self.error_string.set('')
-
-
-        vals = {
-            'R': self.resistance_val,
-            'C': self.capacitance_val,
-            'F': self.frequency_val
-        }
-
         to_calculate = self.select_var.get()
-        print(to_calculate)
-
-        populated_vals = [var.get_absolute()  for key, var in vals.items() if key != to_calculate]
+        populated_vals = [var.get_absolute()  for key, var in self.vars.items() if key != to_calculate]
         try:
             calculated_value = (1 / (2 * pi * populated_vals[0] * populated_vals[1]))
         except ZeroDivisionError:
-            self.error_string.set('Error: Values used for calculation cannot be zero.')
+            # one of the values is zero, so just set the answer 0
+            self.vars[to_calculate].set_absolute(0)
             return
 
         #calculated_value = round(calculated_value * 100) / 100
-        vals[to_calculate].set_absolute(calculated_value)
+        self.vars[to_calculate].set_absolute(calculated_value)
 
 
-    def reset(self):
+    def _reset(self):
         """Reset the form to default"""
 
-        self.resistance_val.set(0, 'kilo')
-        self.capacitance_val.set(0, 'nano')
-        self.frequency_val.set(0, '')
+        self.vars['R'].set(0, 'kilo')
+        self.vars['C'].set(0, 'nano')
+        self.vars['F'].set(0, '')
 
     def _output_selected(self, *_):
 
