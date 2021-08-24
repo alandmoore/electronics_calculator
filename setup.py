7ec0581d4baa6b541c8c0b9c2f0cfc915dd36ab5
Alan Moore <me@alandmoore.com>
2021-08-24 16:15:13 -0500

    Fixes to setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import find_packages, setup
 setup(
     name='electronics-calculator',
     version='0.1',
-    packages=find_packages('electronics_calculator'),
+    packages=['electronics_calculator'],
+    install_requires=['electronics_calculator'],
     author='Alan D Moore',
     author_email='alan@alandmoore.com',
     url='https://github.com/alandmoore/electronics_calculator',
