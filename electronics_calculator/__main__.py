92144d2da6a2b12c87c4f503ddcac6c353186cfd
Alan Moore <me@alandmoore.com>
2021-08-24 16:08:19 -0500

    Added setup.py and __main__.py

---
+++ electronics_calculator/__main__.py
@@ -0,0 +1,8 @@
+from electronics_calculator.application import Application
+
+def main():
+    root = Application()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='electronics-calculator',
+    version='0.1',
+    packages=find_packages('electronics_calculator'),
+    author='Alan D Moore',
+    author_email='alan@alandmoore.com',
+    url='https://github.com/alandmoore/electronics_calculator',
+    entry_points={
+        'console_scripts': [
+            'electronics-calculator = electronics_calculator.__main__:main'
+        ]
+    }
+)
